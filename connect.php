126e675a462ec3845f868cffffd65c701d2700b8
Mayaank Gupta <guptamayaank55@gmail.com>
2019-04-23 13:23:47 +0530

    add to cart

--- connect.php
+++ connect.php
@@ -2,3 +2,20 @@
 $e=mysql_connect("localhost","root","");
 mysql_select_db("spiritual_project",$e) or die("connection problem");
 ?>
+<script>
+$(document).ready(function() {
+    $(".w3ls-cart").click(function(){
+		var id=$(this).attr("id");
+		var my='pid='+id;
+		$.ajax({
+			data:my,
+			type:'post',
+			url:"cart_session.php",
+			success:function(mess){
+				
+				}
+			});
+		
+		});
+});
+</script>
---
+++ view_cart.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Spiritual Store</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Electronic Store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	SmartPhone Compatible web template, free web designs for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+	function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<!-- Custom Theme files -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/fasthover.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //Custom Theme files -->
+<!-- font-awesome icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+<!-- js -->
+<script src="js/jquery.min.js"></script>
+<link rel="stylesheet" href="css/jquery.countdown.css" /> <!-- countdown --> 
+<!-- //js -->  
+<!-- web fonts --> 
+<link href='//fonts.googleapis.com/css?family=Glegoo:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<!-- //web fonts -->  
+<!-- start-smooth-scrolling -->
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+	
+</script>
+<script>
+$(document).ready(function() {
+    $(".w3ls-cart").click(function(){
+		var id=$(this).attr("id");
+		var my='pid='+id;
+		$.ajax({
+			data:my,
+			type:'post',
+			url:"cart_session.php",
+			success:function(mess){
+				alert(mess);
+				}
+			});
+		
+		});
+});
+</script>
+
+<!-- //end-smooth-scrolling --> 
+</head>
+<body>
+	<!-- for bootstrap working -->
+	<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+	<!-- //for bootstrap working -->
+	<!-- header modal -->
+
+<?php
+session_start();
+include("connect.php");
+include('login.php');
+?>
+	<!-- //header -->
+	<!-- navigation -->
+	<div class="navigation">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header nav_2">
+					<button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div> 
+				<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+					<ul class="nav navbar-nav">
+						<li><a href="index.php" class="act">Home</a></li>	
+						<!-- Mega Menu -->
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Products <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Games</a></li>
+								<li><a href="#">Books</a></li>
+								<li><a href="#">Calenders</a></li>
+								<!-- <div class="row">
+									<div class="col-sm-3">
+										<ul class="multi-column-dropdown">
+											<h6>Mobiles</h6>
+											<li><a href="products.html">Mobile Phones</a></li>
+											<li><a href="products.html">Mp3 Players <span>New</span></a></li> 
+											<li><a href="products.html">Popular Models</a></li>
+											<li><a href="products.html">All Tablets<span>New</span></a></li>
+										</ul>
+									</div>
+									<div class="col-sm-3">
+										<ul class="multi-column-dropdown">
+											<h6>Accessories</h6>
+											<li><a href="products1.html">Laptop</a></li>
+											<li><a href="products1.html">Desktop</a></li>
+											<li><a href="products1.html">Wearables <span>New</span></a></li>
+											<li><a href="products1.html"><i>Summer Store</i></a></li>
+										</ul>
+									</div>
+									<div class="col-sm-2">
+										<ul class="multi-column-dropdown">
+											<h6>Home</h6>
+											<li><a href="products2.html">Tv</a></li>
+											<li><a href="products2.html">Camera</a></li>
+											<li><a href="products2.html">AC</a></li>
+											<li><a href="products2.html">Grinders</a></li>
+										</ul>
+									</div>
+									<div class="col-sm-4">
+										<div class="w3ls_products_pos">
+											<h4>30%<i>Off/-</i></h4>
+											<img src="images/1.jpg" alt=" " class="img-responsive" />
+										</div>
+									</div>
+									<div class="clearfix"></div>
+								</div> -->
+							</ul>
+						</li>
+						<li><a href="about.php">About Us</a></li> 
+						<!-- <li class="w3pages"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="icons.html">Web Icons</a></li>
+								<li><a href="codes.html">Short Codes</a></li>     
+							</ul>
+						</li>   -->
+						<li><a href="mail.php">Mail Us</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<!-- //navigation -->
+	<!-- banner -->
+<?php
+$total=0;
+foreach($_SESSION['cart'] as $key=>$value){
+	$pid= $_SESSION['cart'][$key]['productID']."\t";
+	$qty= $_SESSION['cart'][$key]['Quantity']."<br>";
+	$se=mysql_query("select * from products where pid=$pid");
+	$info=mysql_fetch_array($se);
+	echo $info['name']."\t";
+	echo $info['price']*(100-$info['discount'])/100;
+	echo "\t".$qty."\t <br>";
+	$total+=$qty*$info['price']*(100-$info['discount'])/100;
+?>
+
+<?php
+	}
+	echo "<br><br><br>".$total;
+?>
+<?php
+include("footer.php");
+?>
+</body>
+</html>
