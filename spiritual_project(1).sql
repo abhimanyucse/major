5663eb4c1b0f5a9ba3c320f54c617bb2dc010207
Mayaank Gupta <guptamayaank55@gmail.com>
2019-05-04 22:58:13 +0530

    new db update
    transaction_id
    order_id

--- spiritual_project(1).sql
+++ spiritual_project(1).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 09, 2019 at 06:55 PM
+-- Generation Time: May 04, 2019 at 07:26 PM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -54,6 +54,32 @@ CREATE TABLE `customer` (
   `email` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`mid`, `name`, `phone`, `email`) VALUES
+(23, 'm', 0, 'macrock1203@gmail.com'),
+(27, 'mac', 0, 'macrock1203@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gst`
+--
+
+CREATE TABLE `gst` (
+  `cgst` float NOT NULL,
+  `sgst` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `gst`
+--
+
+INSERT INTO `gst` (`cgst`, `sgst`) VALUES
+(0.18, 0.18);
+
 -- --------------------------------------------------------
 
 --
@@ -65,17 +91,21 @@ CREATE TABLE `master` (
   `username` varchar(200) NOT NULL,
   `password` text NOT NULL,
   `status` varchar(1) NOT NULL DEFAULT 'A',
-  `type` text NOT NULL
+  `type` text NOT NULL,
+  `verify` varchar(1) NOT NULL DEFAULT 'F'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `master`
 --
 
-INSERT INTO `master` (`mid`, `username`, `password`, `status`, `type`) VALUES
-(1, 'a@.n', 'm', 'A', 'A'),
-(2, 'm@k.m', 'm', 'A', 'S'),
-(3, 'f@g.m', 'm', 'A', 'S');
+INSERT INTO `master` (`mid`, `username`, `password`, `status`, `type`, `verify`) VALUES
+(1, 'a@.n', 'm', 'A', 'A', 'T'),
+(2, 'm@k.m', 'm', 'A', 'S', 'T'),
+(3, 'f@g.m', 'm', 'A', 'S', 'T'),
+(23, 'macrock1203@gmail.com', 'op', 'A', 'customer', 'T'),
+(26, 'psycjoker@gmail.com', 'mac', 'A', 'S', 'T'),
+(27, 'macrock1203@gmail.com', 'kl', 'A', 'customer', 'T');
 
 -- --------------------------------------------------------
 
@@ -100,16 +130,17 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`pid`, `name`, `photo`, `price`, `seller`, `quantity`, `discount`, `category`, `info`) VALUES
-(9, 'm', '9.jpg', 90, 2, 90, 90, 'game', '1'),
-(10, 'ma', '10.jpg', 90, 2, 98, 90, 'game', '2'),
-(11, 'MAc', '11.jpg', 89, 2, 89, 78, 'game', '3'),
-(12, 'mac', '12.jpg', 89, 3, 78, 78, 'books', 'kl'),
+(9, 'm', '9.jpg', 90, 2, -1, 90, 'game', '1'),
+(10, 'ma', '10.jpg', 90, 2, -1, 90, 'game', '2'),
+(11, 'MAc', '11.jpg', 100, 2, -406, 78, 'game', '3'),
+(12, 'mac', '12.jpg', 89, 3, 77, 78, 'books', 'kl'),
 (13, 'mac1', '13.jpg', 89, 2, 78, 78, 'books', '89'),
 (14, 'mac2', '14.png', 88, 3, 78, 78, 'books', 'gh'),
 (15, 't1', '15.jpg', 88, 3, 78, 78, 'Calender', '67'),
 (16, 't2', '16.jpg', 88, 3, 78, 78, 'Calender', '56'),
-(17, 't3', '17.jpg', 88, 3, 78, 78, 'Calender', 'gh'),
-(18, 'jk', '18.gif', 89, 2, 78, 78, 'books', 'mnmnmnmnmn');
+(17, 't3', '17.jpg', 88, 26, 78, 78, 'Calender', 'gh'),
+(18, 'jk', '18.gif', 89, 26, 100, 78, 'books', 'mnmnmnmnmn'),
+(19, 'Scented Candels', '19.jpg', 100000, 26, 1000, 5, 'books', 'candel');
 
 -- --------------------------------------------------------
 
@@ -132,7 +163,8 @@ CREATE TABLE `seller` (
 
 INSERT INTO `seller` (`mid`, `name`, `organization`, `address`, `phone`, `email`) VALUES
 (2, 'm', 'k', 'k', 909090909, 'p'),
-(3, 'm', 'jk', 'kl', 9090, 'f@g.m');
+(3, 'm', 'jk', 'kl', 9090, 'f@g.m'),
+(26, 'mac', 'io', 'kl', 567890, 'psycjoker@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -144,10 +176,24 @@ CREATE TABLE `transaction` (
   `tid` int(11) NOT NULL,
   `mid` int(11) NOT NULL,
   `pid` int(11) NOT NULL,
+  `orderid` varchar(50) NOT NULL,
+  `transaction_id` varchar(50) NOT NULL,
+  `name` varchar(30) NOT NULL,
   `quantity` int(11) NOT NULL,
   `address` varchar(1000) NOT NULL,
+  `zipcode` int(6) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `date` date NOT NULL,
   `status` varchar(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`tid`, `mid`, `pid`, `orderid`, `transaction_id`, `name`, `quantity`, `address`, `zipcode`, `phone`, `date`, `status`) VALUES
+(33, 27, 11, 'ORDS60941406', '', 'mac', 2, '90', 90, 90, '2019-05-04', ''),
+(34, 27, 11, 'ORDS65140205', '20190504111212800110168715200468183', 'mac', 3, '90', 90, 90, '2019-05-04', '');
 
 --
 -- Indexes for dumped tables
@@ -157,12 +203,14 @@ CREATE TABLE `transaction` (
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
+  ADD PRIMARY KEY (`mid`),
   ADD KEY `mid` (`mid`);
 
 --
 -- Indexes for table `customer`
 --
 ALTER TABLE `customer`
+  ADD PRIMARY KEY (`mid`),
   ADD KEY `mid` (`mid`);
 
 --
@@ -200,17 +248,17 @@ ALTER TABLE `transaction`
 -- AUTO_INCREMENT for table `master`
 --
 ALTER TABLE `master`
-  MODIFY `mid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `mid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
-  MODIFY `tid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `tid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 --
 -- Constraints for dumped tables
 --
